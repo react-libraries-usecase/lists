2a66ebf785c482fda0a2320decde8c91c3032bcf
jafarRezaei <bomber.man87@yahoo.com>
2019-10-15 11:01:54 +0330

    improve react-table examples

--- packages/React-table/src/Sorting/index.js
+++ packages/React-table/src/Sorting/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useTable } from 'react-table';
+import React from 'react'
+import styled from 'styled-components'
+import { useTable, useSortBy } from 'react-table'
 
-import makeData from './makeData';
+import makeData from './makeData'
 
 const Styles = styled.div`
   padding: 1rem;
@@ -31,49 +31,70 @@ const Styles = styled.div`
       }
     }
   }
-`;
+`
 
 function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow
-  } = useTable({
-    columns,
-    data
-  });
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  )
 
-  // Render the UI for your table
+  // We don't want to render all 2000 rows for this example, so cap
+  // it at 20 for this use case
+  const firstPageRows = rows.slice(0, 20)
+
   return (
-    <table {...getTableProps()}>
-      <thead>
+    <>
+      <table {...getTableProps()}>
+        <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              // Add the sorting props to control sorting. For this example
+              // we can add them into the header props
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render('Header')}
+                {/* Add a sort direction indicator */}
+                <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+              </th>
             ))}
           </tr>
         ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(
+        </thead>
+        <tbody {...getTableBodyProps()}>
+        {firstPageRows.map(
           (row, i) =>
             prepareRow(row) || (
               <tr {...row.getRowProps()}>
                 {row.cells.map(cell => {
                   return (
                     <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  );
+                  )
                 })}
               </tr>
             )
         )}
-      </tbody>
-    </table>
-  );
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+    </>
+  )
 }
 
 function App() {
@@ -84,46 +105,46 @@ function App() {
         columns: [
           {
             Header: 'First Name',
-            accessor: 'firstName'
+            accessor: 'firstName',
           },
           {
             Header: 'Last Name',
-            accessor: 'lastName'
-          }
-        ]
+            accessor: 'lastName',
+          },
+        ],
       },
       {
         Header: 'Info',
         columns: [
           {
             Header: 'Age',
-            accessor: 'age'
+            accessor: 'age',
           },
           {
             Header: 'Visits',
-            accessor: 'visits'
+            accessor: 'visits',
           },
           {
             Header: 'Status',
-            accessor: 'status'
+            accessor: 'status',
           },
           {
             Header: 'Profile Progress',
-            accessor: 'progress'
-          }
-        ]
-      }
+            accessor: 'progress',
+          },
+        ],
+      },
     ],
     []
-  );
+  )
 
-  const data = React.useMemo(() => makeData(20), []);
+  const data = React.useMemo(() => makeData(2000), [])
 
   return (
     <Styles>
       <Table columns={columns} data={data} />
     </Styles>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/React-table/stories/index.story.js
+++ packages/React-table/stories/index.story.js
@@ -2,8 +2,49 @@ import React, { lazy, Suspense } from 'react';
 import { storiesOf } from '@storybook/react';
 
 storiesOf('React-table', module)
+
   .add('Router', () => {
     const BasicForm = lazy(() => import('../src/Basic/index'));
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BasicForm />
+      </Suspense>
+    );
+  })
+  .add('Sorting', () => {
+    const BasicForm = lazy(() => import('../src/Sorting/index'));
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BasicForm />
+      </Suspense>
+    );
+  })
+  .add('Filtering', () => {
+    const BasicForm = lazy(() => import('../src/Filtering/index'));
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BasicForm />
+      </Suspense>
+    );
+  })
+  .add('Grouping', () => {
+    const BasicForm = lazy(() => import('../src/Grouping/index'));
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BasicForm />
+      </Suspense>
+    );
+  })
+  .add('Pagination', () => {
+    const BasicForm = lazy(() => import('../src/Pagination/index'));
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BasicForm />
+      </Suspense>
+    );
+  })
+  .add('Complex', () => {
+    const BasicForm = lazy(() => import('../src/Complex/index'));
     return (
       <Suspense fallback={<div>Loading...</div>}>
         <BasicForm />
